b83ae49c464a60278cdcaae53718ae790275d971
NATHALIA VENTURA KAWAKAMI <23111822@CFP127ALUNO.LOCAL>
2024-04-02 10:11:22 -0300

    insanidades

--- js/main.js
+++ js/main.js
@@ -52,17 +52,69 @@ const getTraducao = async() =>{
         console.log(traducao)
 
         textT.textContent = traducao.responseData.translatedText
+
+        var listenBtn = document.getElementById('btnListen')
+listenBtn.addEventListener('click', async function (){
+
+    if('speechSynthesis' in window){
+
+        const translated = traducao.responseData.translatedText
+        const listenText = new SpeechSynthesisUtterance(translated)
+
+        speechSynthesis.speak(listenText)
+    
+
+    }else{
+        alert("navegador não compatível")
+    }
+})
+    }
+    // função para falar o texto traduzido
+}
+
+var speakBtn = document.getElementById('btnSpeak')
+function callNathalia(){
+
+    var transcription = document.getElementById('portugues')
+
+    if(window.SpeechRecognition || window.webkitSpeechRecognition || window.mozSpeechRecognition || window.msSpeechRecognition){
+       
+        const recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition || window.mozSpeechRecognition || window.msSpeechRecognition)();
+        recognition.lang = 'pt-BR';
+        recognition.continuos = true
+        // for (let country_code in testeLingua) {
+        //     recognition.lang = testeLingua[country_code]
+        // }
+        recognition.onresult = function(event) {
+            // Get the transcript of the recognized speech
+            const transcript = event.results[event.results.length - 1][0].transcript;
+            console.log('You said:', transcript);
+        }
+
+        try{
+            recognition.start()
+        }catch(erro){
+            console.log(erro)
+        }
+
+        recognition.addEventListener('result', function(event){
+            var result = event.results[0][0].transcript
+            transcription.value = result
+        })
+
+    }else{
+        console.log('navegador não compatível')
     }
 }
+callNathalia()
+
 
 const selects = document.querySelectorAll('select')
 
 selects.forEach((tag, langs) => {
-    console.log(testeLingua)
     for (let country_code in testeLingua) {      
         let selected = langs == 0 ? country_code == "en-GB" ? "selected" : "" : country_code == "pt-PT" ? "selected" : "";
         let option = `<option ${selected} value="${country_code}">${testeLingua[country_code]}</option>`;
-        console.log(option)
         tag.insertAdjacentHTML("beforeend", option);
     }
 })
